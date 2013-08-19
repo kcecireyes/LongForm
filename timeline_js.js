fa7f5fa6b9e25152887ab9232b123921a9116123
Cecilia Reyes <cecilia_reyes@wgbh.org>
2013-08-19 16:08:04 -0400

    Visualization no.2 done

--- timeline_js.js
+++ timeline_js.js
@@ -6,33 +6,33 @@
 //load data
 var dataset = [];
 var color = d3.scale.ordinal()
-    .range(["steelblue", "steelblue", "steelblue", "steelblue", "steelblue", 
-      "#FAF0E6", "#FAF0E6", "#FAF0E6", "#FAF0E6", "#FAF0E6", "#FAF0E6", "#FAF0E6",
-      "#F08080","#F08080","#F08080","#F08080","#F08080"]);
+.range(["steelblue", "steelblue", "steelblue", "steelblue", "steelblue", 
+  "#FAF0E6", "#FAF0E6", "#FAF0E6", "#FAF0E6", "#FAF0E6", "#FAF0E6", "#FAF0E6",
+  "#F08080","#F08080","#F08080","#F08080","#F08080"]);
 
 d3.csv("data/faux.csv", function (data) { // total_faculty_timeline.csv
 	var yr = 2012; 
 	var facultyData = data;
 	yearlyData(facultyData, yr);
 
-function yearlyData (data, yr) {
-	var year = String(yr);
-	dataset = data.map(function(d) { return [+d[year]]; });
-	makeDots();
-}
+  function yearlyData (data, yr) {
+   var year = String(yr);
+   dataset = data.map(function(d) { return [+d[year]]; });
+   makeDots();
+ }
 
-function makeDots () {
+ function makeDots () {
   $(".dots").remove(); //remove existing dots
 
-	dot = svg.append("g")
-	.attr("class", "dots")
-	.selectAll(".dot")
-	.data(data)
-	.enter().append("circle")
-	.attr("class", "dot")
-	.style("fill", function (d,i) {return color(i) } )
-	.attr("r", function(d,i){ return Math.sqrt(dataset[i]); })
-	.attr("cx", function(d,i) { return i * 55; })
+  dot = svg.append("g")
+  .attr("class", "dots")
+  .selectAll(".dot")
+  .data(data)
+  .enter().append("circle")
+  .attr("class", "dot")
+  .style("fill", function (d,i) {return color(i) } )
+  .attr("r", function(d,i){ return Math.sqrt(dataset[i]); })
+  .attr("cx", function(d,i) { return i * 55; })
 	.attr("cy", function() { return  100; }); //Math.random() * 300;
 }
 
@@ -59,27 +59,106 @@ function enableInteraction() {
   .clamp(true);
   
   overlay
-    .on("mouseover", mouseover)
-    .on("mouseout", mouseout)
-    .on("mousemove", mousemove)
-    .on("touchmove", mousemove);
+  .on("mouseover", mouseover)
+  .on("mouseout", mouseout)
+  .on("mousemove", mousemove)
+  .on("touchmove", mousemove);
 
-    function mouseover() {
-      label.classed("active", true);
-    }
+  function mouseover() {
+    label.classed("active", true);
+  }
 
-    function mouseout() {
-      label.classed("active", false);
-    }
+  function mouseout() {
+    label.classed("active", false);
+  }
 
-    function mousemove() {
-     displayYear(Math.round(yearScale.invert(d3.mouse(this)[0])));
-   }
+  function mousemove() {
+   displayYear(Math.round(yearScale.invert(d3.mouse(this)[0])));
  }
+}
 
 // Updates the display to show the specified year.
 function displayYear(year) {
   yearlyData(facultyData, year);
   label.text(Math.round(year));
 }
-});		
+});
+
+//PIE CHART
+//Built with help of mbostock's examples
+
+
+var radius = Math.min(960, 500) / 2,
+dept = ["Instruction", "Research", "Libraries", "Administration", "Support_Staff"];
+
+var colorS = d3.scale.category20();
+
+var pie = d3.layout.pie()
+.value(function(d) { return d.Asian; })
+.sort(null);
+
+var arc = d3.svg.arc()
+.innerRadius(radius - 100)
+.outerRadius(radius - 20);
+loadData();
+
+function loadData () {
+  console.log("why won't you load? :( ");
+    d3.csv("data/Race.csv", function(raceData){
+      var path = svg2.datum(raceData).selectAll("path")
+      .data(pie)
+      .enter().append("path")
+      .attr("fill", function(d, i) { return colorS(i); })
+      .attr("id", function (d,i) { return dept[i]; })
+      .attr("d", arc)
+  .each(function(d) { this._current = d; }); // store the initial angles
+
+  d3.selectAll("input")
+  .on("change", change);
+
+  var timeout = setTimeout(function() {
+    d3.select("input[value=\"Black\"]").property("checked", true).each(change);
+    d3.select("input[value=\"Hispanic\"]").property("checked", true).each(change);
+    d3.select("input[value=\"Native\"]").property("checked", true).each(change);
+    d3.select("input[value=\"Pacific\"]").property("checked", true).each(change);
+    d3.select("input[value=\"Multiple\"]").property("checked", true).each(change);
+    d3.select("input[value=\"Undisclosed\"]").property("checked", true).each(change);
+    d3.select("input[value=\"White\"]").property("checked", true).each(change);
+    d3.select("input[value=\"Non_Resident\"]").property("checked", true).each(change);
+  }, 2000);
+
+  function change() {
+    var value = this.value;
+    clearTimeout(timeout);
+  pie.value(function(d) { return d[value]; }); // change the value function
+  path = path.data(pie); // compute the new angles
+  path.transition().duration(750).attrTween("d", arcTween); // redraw the arcs
+  
+}
+});
+
+}
+
+function type(d) {
+  d.Asian = +d.Asian;
+  d.Black = +d.Black;
+  d.Hispanic = +d.Hispanic;
+  d.Native = +d.Native;
+  d.Pacific = +d.Pacific;
+  d.Multiple = +d.Multiple;
+  d.Undisclosed = +d.Undisclosed;
+  d.White = +d.White;
+  d.Non_Resident = +d.Non_Resident;
+  return d;
+}
+
+// Store the displayed angles in _current.
+// Then, interpolate from _current to the new angles.
+// During the transition, _current is updated in-place by d3.interpolate.
+function arcTween(a) {
+  var i = d3.interpolate(this._current, a);
+  this._current = i(0);
+  return function(t) {
+    return arc(i(t));
+  };
+}		
